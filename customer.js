6d87b35d61b5bc5913be068e86cc5a995905b930
Hoo Man <hoomanfor@gmail.com>
2019-12-01 13:43:35 -0500

    logging product id, name, price from the products table

--- customer.js
+++ customer.js
@@ -14,6 +14,14 @@ connection.connect(function(error) {
     if (error) throw error;
     console.log("Connected!");
 });
+ 
+function displayProducts() {
+    connection.query("SELECT * FROM products", function(error, response) {
+        if (error) throw error;
+        response.forEach(function(element) {
+            console.log(element.id + " - " + element.product_name + " - " + element.price);
+        })
+    })
+}
 
-
-
+displayProducts()
