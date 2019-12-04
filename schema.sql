58e160c934fe0e9c21e89cdacc78d45cd42dd537
Hoo Man <hoomanfor@gmail.com>
2019-12-04 17:55:26 -0500

    added product_sales to products table

--- schema.sql
+++ schema.sql
@@ -9,6 +9,7 @@ CREATE TABLE products (
     department_name VARCHAR(75) NOT NULL,
     price DECIMAL(10,2) NOT NULL,
     stock_quantity INTEGER(10) NOT NULL,
+    product_sales DECIMAL(10,2) NOT NULL,
     PRIMARY KEY (id)
 );
 
--- seeds.sql
+++ seeds.sql
@@ -1,14 +1,14 @@
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Macbook Air", "Computing", 899.99, 25),
-('Macbook Pro 13"', "Computing", 1099.99, 25),
-('Surface Laptop 3 13.5"', "Computing", 999.00, 25),
-('Surface Book 2 15"', "Computing", 1999.00, 25),
-("AirPods Pro", "Audio", 249.99, 25),
-("QuiteComfore 35 II", "Audio", 279.99, 25),
-("iPhone 11", "Mobile", 699.99, 25),
-("Galaxy S10", "Mobile", 699.99, 25),
-("Switch 32GB Console", "Gaming", 299.99, 25),
-("Xbox One X 1TB", "Gaming", 349.99, 25);
+INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+VALUES ("Macbook Air", "Computing", 899.99, 25, 0),
+('Macbook Pro 13"', "Computing", 1099.99, 25, 0),
+('Surface Laptop 3 13.5"', "Computing", 999.00, 25, 0),
+('Surface Book 2 15"', "Computing", 1999.00, 25, 0),
+("AirPods Pro", "Audio", 249.99, 25, 0),
+("QuiteComfore 35 II", "Audio", 279.99, 25, 0),
+("iPhone 11", "Mobile", 699.99, 25, 0),
+("Galaxy S10", "Mobile", 699.99, 25, 0),
+("Switch 32GB Console", "Gaming", 299.99, 25, 0),
+("Xbox One X 1TB", "Gaming", 349.99, 25, 0);
 
 INSERT INTO departments (department_name, over_head_costs)
 VALUES ("Computing", 2000.00),
